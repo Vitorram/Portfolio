2f6f56af71468380792e8f819837d15145abbcda
Vitor rams <161902126+Vitorram@users.noreply.github.com>
2025-01-13 17:04:44 +0000

    iaea

--- js.js
+++ js.js
@@ -1,4 +1,21 @@
 function showProjectDetails() {
-  const projectDetails = document.getElementById('detalhesProjeto');
-  projectDetails.classList.toggle('hidden');
+  // Esconde a div de projetos
+  document.querySelector('.projetos').classList.add('hidden');
+  
+  // Exibe a div de detalhes do projeto
+  document.getElementById('detalhesProjeto').classList.remove('hidden');
+  
+  // Exibe a imagem do corpo do projeto
+  document.querySelector('.foto-corpo').classList.remove('hidden');
+}
+
+function goBack() {
+  // Exibe a div de projetos
+  document.querySelector('.projetos').classList.remove('hidden');
+  
+  // Esconde a div de detalhes do projeto
+  document.getElementById('detalhesProjeto').classList.add('hidden');
+  
+  // Esconde a imagem do corpo do projeto
+  document.querySelector('.foto-corpo').classList.add('hidden');
 }
